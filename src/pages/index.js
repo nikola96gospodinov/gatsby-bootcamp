c540b9db29f60f093709fadd2157e879b637c1da
Nikola <nikola96gospodinov@gmail.com>
2020-06-23 21:23:26 +0100

    Final Gatsby Site

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+const IndexPage = () => {
+    return (
+        <Layout>
+            <Head title="Home"/>
+            <h1>Hello.</h1>
+            <h2>I'm Nik, a full-stack developer living in Bristol.</h2>
+            <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
+        </Layout>
+    )
+}
+
+export default IndexPage
